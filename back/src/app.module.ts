de82548ea0d03a3376473d9459446a7223e75ab2
juanllano3311 <70298836+juanllano3311@users.noreply.github.com>
2022-02-22 23:36:52 -0500

    :tada: inicio de proyecto

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AdminModule } from './apis/admin/admin.module';
+import { MemberModule } from './apis/member/member.module';
+
 
 @Module({
-  imports: [],
+  imports: [AdminModule, MemberModule],
   controllers: [AppController],
   providers: [AppService],
 })
